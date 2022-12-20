2dde5ed380b7d579bd94d15c31b18f164809e0a7
Nelfyk <ruslan-burduzhan@mail.ru>
2022-12-20 17:52:57 +0500

    урок 6.6
    #Получение списка всех работников

---
+++ src/main/java/com/ruslanburduzhan/spring/rest/controller/MyRESTController.java
@@ -0,0 +1,24 @@
+package com.ruslanburduzhan.spring.rest.controller;
+
+import com.ruslanburduzhan.spring.rest.entity.Employee;
+import com.ruslanburduzhan.spring.rest.service.EmployeeService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api")
+public class MyRESTController {
+
+    @Autowired
+    EmployeeService employeeService;
+
+    @GetMapping("/employees")
+    public List<Employee> showAllEmployees(){
+        List<Employee> employeeList = employeeService.getAllEmployees();
+        return employeeList;
+    }
+}
---
+++ src/main/java/com/ruslanburduzhan/spring/rest/service/EmployeeService.java
@@ -0,0 +1,15 @@
+package com.ruslanburduzhan.spring.rest.service;
+
+import com.ruslanburduzhan.spring.rest.entity.Employee;
+
+import java.util.List;
+
+public interface EmployeeService {
+    List<Employee> getAllEmployees();
+
+    void saveEmployee(Employee employee);
+
+    Employee getEmployee(int id);
+
+    void deleteEmployee(int id);
+}
