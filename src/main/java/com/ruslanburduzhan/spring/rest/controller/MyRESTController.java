7f60ee66c6bb76e02def94b3d8a59190f56936cc
Nelfyk <ruslan-burduzhan@mail.ru>
2022-12-21 14:30:21 +0500

    урок 6.12
    #Завершили все CRUD команды.

--- src/main/java/com/ruslanburduzhan/spring/rest/controller/MyRESTController.java
+++ src/main/java/com/ruslanburduzhan/spring/rest/controller/MyRESTController.java
@@ -1,11 +1,10 @@
 package com.ruslanburduzhan.spring.rest.controller;
 
 import com.ruslanburduzhan.spring.rest.entity.Employee;
+import com.ruslanburduzhan.spring.rest.exception_handling.NoSuchEmployeeException;
 import com.ruslanburduzhan.spring.rest.service.EmployeeService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -17,8 +16,36 @@ public class MyRESTController {
     EmployeeService employeeService;
 
     @GetMapping("/employees")
-    public List<Employee> showAllEmployees(){
-        List<Employee> employeeList = employeeService.getAllEmployees();
-        return employeeList;
+    public List<Employee> showAllEmployees() {
+        return employeeService.getAllEmployees();
+    }
+
+    @GetMapping("/employees/{id}")
+    public Employee getEmployee(@PathVariable int id) {
+        Employee employee = employeeService.getEmployee(id);
+        if (employee == null) {
+            throw new NoSuchEmployeeException("There is no employee with id = " + id + " in Database");
+        }
+        return employee;
+    }
+
+    @PostMapping("/employees")
+    public Employee addNewEmployee(@RequestBody Employee employee){
+        employeeService.saveEmployee(employee);
+        return employee;
+    }
+
+    @PutMapping("/employees")
+    public Employee updateEmployee(@RequestBody Employee employee){
+        employeeService.saveEmployee(employee);
+        return employee;
+    }
+
+    @DeleteMapping("/employees/{id}")
+    public String deleteEmployee(@PathVariable int id){
+        if(employeeService.getEmployee(id)==null){
+            throw new NoSuchEmployeeException("There is no Employee with ID = "+id);
+        }
+        return "Employee with ID = \"+id+\" was deleted.";
     }
 }
---
+++ src/main/java/com/ruslanburduzhan/spring/rest/exception_handling/GlobalExceptionHandler.java
@@ -0,0 +1,27 @@
+package com.ruslanburduzhan.spring.rest.exception_handling;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+// Глобальный класс для обработчиков ошибок, чтобы не описывать их в каждом контроллере.
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+    // not found employee handler
+    @ExceptionHandler
+    public ResponseEntity<EmployeeIncorrectData> handleException(NoSuchEmployeeException exception) {
+        EmployeeIncorrectData data = new EmployeeIncorrectData();
+        data.setInfo(exception.getMessage());
+        return new ResponseEntity<>(data, HttpStatus.NOT_FOUND);
+    }
+
+    // incorrect input handler
+    @ExceptionHandler
+    public ResponseEntity<EmployeeIncorrectData> handleException(Exception exception) {
+        EmployeeIncorrectData data = new EmployeeIncorrectData();
+        data.setInfo(exception.getMessage());
+        return new ResponseEntity<>(data, HttpStatus.BAD_GATEWAY);
+    }
+}
